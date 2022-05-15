9c4bf8b7b3e3bf85c3832314bc6463a41869f8b8
martin fernadnez <martinfernandez@MacBook-Pro-de-martin.local>
2022-05-15 15:44:25 -0700

    cambio a app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{backgroundColor: 'red'}}>
+        <h2 style={{color: 'white', fontSize: '40px'}}>
+          Test Deploy
+        </h2>
+      </div>
     </div>
   );
 }
