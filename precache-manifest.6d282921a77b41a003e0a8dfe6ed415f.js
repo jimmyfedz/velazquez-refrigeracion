b4d242032f1bfdf304f22d3bc2257e5fa2f52665
martin fernandez <martinfernandez@MacBook-Pro-de-martin.local>
2022-05-15 17:59:52 -0700

    Updates

---
+++ precache-manifest.6d282921a77b41a003e0a8dfe6ed415f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b251c7200a766c264a2",
+    "url": "/velazquez-refrigeracion/static/css/main.5e292bb0.chunk.css"
+  },
+  {
+    "revision": "9b251c7200a766c264a2",
+    "url": "/velazquez-refrigeracion/static/js/main.9b251c72.chunk.js"
+  },
+  {
+    "revision": "2b447e1be3e7dd9deafc",
+    "url": "/velazquez-refrigeracion/static/js/1.2b447e1b.chunk.js"
+  },
+  {
+    "revision": "96f7c7ef76a7584837e4",
+    "url": "/velazquez-refrigeracion/static/js/2.96f7c7ef.chunk.js"
+  },
+  {
+    "revision": "249a66da0f0cdd5c23f4",
+    "url": "/velazquez-refrigeracion/static/js/runtime~main.249a66da.js"
+  },
+  {
+    "revision": "38ecd1790ca4be6f05a6060653df7c50",
+    "url": "/velazquez-refrigeracion/index.html"
+  }
+];
