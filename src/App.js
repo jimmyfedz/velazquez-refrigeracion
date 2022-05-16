6d3993c912aee655263bbc24ef2af641382d0642
martin fernandez <martinfernandez@MacBook-Pro-de-martin.local>
2022-05-15 17:57:57 -0700

    connection to graphql endpoint in hasura db

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
+import React from 'react';
+import {useQuery, gql} from '@apollo/client';
 
 function App() {
+  const QUERY = gql`query MyQuery {
+    users {
+      age
+      first_name
+      id
+      last_name
+    }
+  }
+  `;
+  const {data, loading, error} = useQuery(QUERY);
+  console.log(`data: ${data}, loading: ${loading}, error: ${error}`);
   return (
     <div>
-      <div style={{backgroundColor: 'red'}}>
+      {!loading && data ? <div>
+        <ul>
+          {data.users.map((e,i)=><li key={i}>{e.first_name}</li>)}
+          </ul> </div>: <div style={{backgroundColor: 'red'}}>
         <h2 style={{color: 'white', fontSize: '40px'}}>
-          Test Deploy
+          Loading ...
         </h2>
-      </div>
+      </div>}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+} from "@apollo/client";
 
+const client = new ApolloClient({
+  uri: 'http://34.70.32.73/v1/graphql',
+  headers: {
+    "content-type": "application/json",
+    "x-hasura-admin-secret": "4SaLFk^rvx5^"
+},
+  cache: new InMemoryCache()
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
+  </ApolloProvider>,
   </React.StrictMode>
 );
 
